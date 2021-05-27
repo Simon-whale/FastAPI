529379cbe9d6fc1c1879be9feebfcd42de930807
Simon-whale <83454878+Simon-whale@users.noreply.github.com>
2021-05-27 22:40:03 +0100

    Adding in an Errorring router so we can see how to return different status codes
    Adding in response models and error models etc
    updated the readme so that we know raiseing errors is in the API

---
+++ Router/ErrorHandling.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+# TODO: find out if there is a rule of when to encapsulate another class
+#       rather than seperate it out into it's own class
+class Item(BaseModel):
+     id: str
+     value: str
+
+
+class Message(BaseModel):
+     message: str
+
+
+router = APIRouter()
+
+
+@router.get('/blowsup/{item_no}', tags=["Errors"])
+def she_gonna_blow(item_no: int):
+    if item_no >= 100:
+        # Here on purpose we raise an exception if the ID is equal
+        # or greater too 100
+        raise HTTPException(status_code=404, detail="Id is not found")
+
+    return {"It didn't break it": id}
+
+
+@router.get("/break/{item_id}", response_model=Item, responses={404: {"model": Message}}, tags=["Errors"])
+def it_go_boom(item_no: int):
+    """
+    This endpoint is showing that you can set a format for the response message and an error message
+    """
+    if item_no == 100:
+        return {"id": "foo", "value": "It didn't go BOOM"}
+    else:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
